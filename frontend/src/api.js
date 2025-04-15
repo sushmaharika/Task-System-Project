246f6743fe4c15e13a4addf047727290466b982a
sushmaharika <137254731+sushmaharika@users.noreply.github.com>
2025-04-15 13:07:53 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,11 @@
+// src/api.js
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/api";
+
+const API = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default API;
 
